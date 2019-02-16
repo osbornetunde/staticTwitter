8a7b2ccb74c4122de5047401aeb7d78feb0be3d5
Tunde Osborne <osbornetunde@gmail.com>
2019-02-17 00:32:53 +0100

    Updates

---
+++ precache-manifest.a13a91efac3d19665d0c05429383a7ab.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "95dadc4eecfaa83f18c4",
+    "url": "/staticTwitter/static/js/runtime~main.95dadc4e.js"
+  },
+  {
+    "revision": "c593ef41db388256543b",
+    "url": "/staticTwitter/static/js/main.c593ef41.chunk.js"
+  },
+  {
+    "revision": "945ac6105096091d7649",
+    "url": "/staticTwitter/static/js/1.945ac610.chunk.js"
+  },
+  {
+    "revision": "c593ef41db388256543b",
+    "url": "/staticTwitter/static/css/main.9b82e69c.chunk.css"
+  },
+  {
+    "revision": "37fa3e66672617f8bf29d7b28041e3b2",
+    "url": "/staticTwitter/index.html"
+  }
+];
